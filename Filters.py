c50d3a6f6d37e25cd8092023cb966b30539ba40d
ivanrj7j <ivanrj7j@gmail.com>
2023-08-29 12:04:06 +0530

    added rotation filter

--- Filters.py
+++ Filters.py
@@ -32,7 +32,47 @@ class Filter:
         raise NotImplementedError("This method is meant to be implemented by the child")
     
 
+class Rotate(Filter):
+    def __init__(self, maxAngle:int, rotateAnchor:tuple[float, float]) -> None:
+        """
+        Rotates the image in the given angle from the rotate angle
+        
+        Keyword arguments:
+        maxAngle (int) -- Maximum angle of rotation
+        rotateAnchor (tuple) -- Anchor of rotation values should be in range [0, 1] in form (x, y)
+        Return: return_description
+        """
+        
+        super().__init__()
+        if not (isinstance(maxAngle, float) or isinstance(maxAngle, int)):
+            raise TypeError("maxAngle should be float or int")
+        self.maxAngle = round(maxAngle)
 
+        if not (isinstance(rotateAnchor, tuple) or isinstance(rotateAnchor, list)):
+            raise TypeError("rotateAnchor should be tuple or list")
+        
+        if len(rotateAnchor) != 2:
+            raise ValueError(f"rotateAnchor should have 2 elements, x and y component not {len(rotateAnchor)}")
+        
+        for value, i in enumerate(rotateAnchor):
+            if (not isinstance(value, float)) and (value != 0 or value != 1):
+                raise TypeError("Rotate Anchor should either be float or 0 or 1")
+            if value < 0 or value > 1:
+                raise ValueError(f"The value of the {['x', 'y'][i]} element should be in range [0, 1]")
+        
+        self.rotateAnchor = rotateAnchor
+
+    def forward(self, image: ndarray) -> ndarray:
+        """
+        Rotates the image by the given angle when initializing the object
+        
+        Keyword arguments:
+        image -- The ndarray of the image to be rotated
+        Return: The ndarray of the rotated image
+        """
+        M = cv2.getRotationMatrix2D(np.multiply(image.shape[:2], self.rotateAnchor), self.rand.randint(0, self.maxAngle))
+
+        return cv2.warpAffine(image, M, image.shape[:2])
 
         
         
