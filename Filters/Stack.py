858a2961e985528e478ab22fe520d123f6948a26
ivanrj7j <ivanrj7j@gmail.com>
2023-09-08 20:50:38 +0530

    added stacks

--- Filters/Stack.py
+++ Filters/Stack.py
@@ -1,5 +1,29 @@
+from COCO import COCO
 from Filters import Filter
+from numpy import ndarray
 
-class Stack:
-    def __init__(self) -> None:
-        pass
+class Stack(Filter):
+    """
+    Stacks multiple filters together
+    """
+    def __init__(self, filters:list[Filter]) -> None:
+        if not all(issubclass(type(x), Filter) for x in filters):
+            raise TypeError("All the elements of the filters list should be a subclass of filters")
+        
+        self.filters = filters
+
+    def forward(self, image: ndarray) -> ndarray:
+        current = image
+        for f in self.filters:
+            current = f.forward(current)
+
+        return current
+    
+    def forwardWithBBox(self, image: ndarray, bBoxes: list[COCO]):
+        currentImage = image
+        currentBBox = bBoxes
+
+        for f in self.filters:
+            currentImage, currentBBox = f.forwardWithBBox(currentImage, currentBBox)
+
+        return currentImage, currentBBox       
